4a90c0039ee5e2b44a6bfb9acd165275b5c2848e
michel <michel@michel-PC>
2015-07-28 23:36:32 -0500

    Segunda correcion Ht2

---
+++ hoja de trabajo 2/src/hoja/de/trabajo/pkg2/stackVector.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja.de.trabajo.pkg2;
+
+/**
+ *
+ * @author michel
+ */
+import java.util.*;
+public class stackVector <E> implements Stack <E>{
+
+	public Vector <E>dato;
+
+	public stackVector (){
+	
+	dato= new Vector<E>();
+	
+	}
+
+	public void push(E item){
+	 
+		dato.add(item);
+	}
+	public E pop(){
+	   
+	   return dato.remove(size()-1);
+	}
+
+	public E peek(){
+	
+		return dato.get(size() - 1);
+	}
+
+   
+   public boolean empty(){
+	     
+		return size()==0;
+   }
+   
+	public int size(){
+	
+	   return dato.size();
+	}
+
+}
