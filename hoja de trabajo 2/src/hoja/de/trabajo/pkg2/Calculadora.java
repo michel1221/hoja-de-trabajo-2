c12ef790d3bc5ab406591177f2f76ab12efb1c66
michel <michel@michel-PC>
2015-07-28 23:38:25 -0500

    Cuarta correcion Ht2

---
+++ hoja de trabajo 2/src/hoja/de/trabajo/pkg2/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja.de.trabajo.pkg2;
+
+/**
+ *
+ * @author michel
+ */
+public interface Calculadora{
+
+	public void setString(String linea);
+	
+	public void meterVector ();
+	
+	public boolean calcularVector();
+	
+	public int getResultado ();
+	
+
+
+}
